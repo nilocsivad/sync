4dc85da374a5ee51061e0ed3ceef7adbf091f489
dfs-1202 <dfs-1202@dfs-1202-PC>
2017-12-21 12:26:33 +0800

    自动映射 Linux/Mac 的根目录到 /synv-rs

--- src/main/java/com/iam_vip/sync/rs/u/PathUtil.java
+++ src/main/java/com/iam_vip/sync/rs/u/PathUtil.java
@@ -78,6 +78,22 @@ public class PathUtil {
 			int port = request.getServerPort();
 			String http = request.getScheme() + "://" + request.getServerName() + (port == 80 ? "" : ":" + port);
 			String prefix = "sync-rs";
+			
+			{ /// 动态创建 Tomcat 目录映射 ///
+				
+				String sRealPath = request.getSession().getServletContext().getRealPath("/");
+				File fPath1 = new File(sRealPath);
+				
+				File fRootXml = new File(fPath1.getParentFile().getParentFile(), "conf\\Catalina\\localhost\\" + prefix + ".xml");
+				
+				if (fRootXml.exists() == false) {
+					fRootXml.createNewFile();
+					FileWriter fw = new FileWriter(fRootXml);
+					fw.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?> \r\n");
+					fw.write("<Context docBase=\"/\" /> \r\n");
+					fw.close();
+				}
+			}
 
 			String to = http + prefix + invocable.invokeFunction("enc", path).toString().replace("%2F", "/");
 
